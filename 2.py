c29104860c7a91c83f3f9ea49cbd3408f21c182c
David Poblador i Garcia <david@poblador.com>
2019-02-08 16:46:20 +0100

    add solution to problem 2

---
+++ 2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def fib():
+    i = 0
+    j = 1
+    while True:
+        yield j
+        i, j = j, j+i
+
+def fibmax(maxnum):
+    for a in fib():
+        if a >= maxnum:
+            break
+        
+        yield a
+
+b = sum(a for a in fibmax(4000000) if not a%2)
+
+print(b)
