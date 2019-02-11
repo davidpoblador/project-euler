06647f71eb378f841e56bd835a6412e48018e8ab
David Poblador i Garcia <david@poblador.com>
2019-02-11 21:42:21 +0100

    fix style

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 def fib():
     i = 0
     j = 1
@@ -7,11 +8,12 @@ def fib():
         yield j
         i, j = j, j+i
 
+
 def fibmax(maxnum):
     for a in fib():
         if a >= maxnum:
             break
-        
+
         yield a
 
 b = sum(a for a in fibmax(4000000) if not a%2)
