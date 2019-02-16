aaf0aacc8c34ed5dc3b8d41d4d7a8f4a7b33f35b
David Poblador i Garcia <david@poblador.com>
2019-02-16 23:27:12 +0100

    solve 9

--- 9.py
+++ 9.py
@@ -1,39 +1,14 @@
 #!/usr/bin/env python
 
-from functools import lru_cache
-
-BELOW = 2000000
+MAX = 1000
 
 
-@lru_cache(maxsize=None)
-def isprime(n):
-    if n == 2:
-        return True
-    if n < 2 or n % 2 == 0:
-        return False
+def solve():
+    for a in range(1, MAX):
+        for b in range(a, MAX):
+            c = MAX - a - b
+            if a**2 + b**2 == c**2:
+                return (a*b*c)
 
-    for i in range(3, n//2, 2):
-        if n % i == 0:
-            return False
-
-    return True
-
-i = 2
-prime_sum = 0
-while i < BELOW:
-    if not i % 20000:
-        print(i)
-
-    if i > 5:
-        b = i % 10
-        if b in {2, 4, 5, 6, 8, 0}:
-            i += 1
-            continue
-
-    if isprime(i):
-        prime_sum += i
-
-    i += 1
-
-
-print(prime_sum)
+o = solve()
+print(o)
